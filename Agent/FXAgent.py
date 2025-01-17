7dcfe6ade303864d290b0b10ca1f362e8b96f120
Simeon Akanbi <simeonakanbi99@gmail.com>
2025-01-17 22:05:36 +0100

    Refactor FXAgent prompt for clarity and update main function to run synchronously

--- Agent/FXAgent.py
+++ Agent/FXAgent.py
@@ -21,10 +21,10 @@ gpt_model = OpenAIModel('gpt-3.5-turbo', api_key=os.getenv('OPENAI_API_KEY'))
 fx_agent = Agent(
     model=gpt_model,
     system_prompt=(
-        "Your name is Felix. You are a foreign exchange trader. You are given a currency pair and a time frame."
+        "Your name is Felix. You are a Forex trader. You are given a currency pair and a time frame."
         "You need to predict the price of the currency pair at the end of the time frame."
         "Strictly use the information provided from the tools"
-        "You decide whether to buy or sell or hold a bit on the currency pair. Then predict when the price will reach the target price. Stop loss and take profit are optional."
+        "You decide whether to buy or sell or hold a bit on the currency pair."
         "If your decision is to hold, provide a reason for your decision. Then predict when the price should be okay for a buy or sell one should place an entry order"
         "Currency pairs: BTCUSD"
         "You make use of the tools provided to make your decision and provide a reason for your decision."
--- main.py
+++ main.py
@@ -2,17 +2,12 @@ from Agent.FXAgent import fx_agent
 import asyncio
 
 
-async def main():
-    async with fx_agent:
-        # Sample run
-        result = await fx_agent.run("Hey Felix, can you login to my metatrader app, and get the information of the pair BTCUSDm and place a trade if you find a trading opportunity")
-        print(result)
+def main():
+    # Sample run
+    result = fx_agent.run_sync("Hey Felix, can you login to my metatrader app, and get the information of the pair BTCUSDm and place a trade if you find a trading opportunity")
+    print(result)
         # result = await fx_agent.run_sync("use_indicators(pair='BTCUSDm')")
         # print(result)
 
 
-
-if __name__ == "__main__":
-    asyncio.run(main())
-    # fx_agent.run_sync("get_pair_info(pair='BTCUSDm', timeframe=15)")
-# fx_agent.run_sync("")
+main()
