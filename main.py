70b986d56938ceee23f70e58e5a023bf100cd52f
Simeon Akanbi <simeonakanbi99@gmail.com>
2025-01-18 01:09:42 +0100

    Refactor main function prompt for clarity and conciseness in trading request

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ load_dotenv()
 def main():
     # Sample run
     dependencies = Deps(pair='BTCUSDm', timeframe=15, login=int(os.getenv("LOGIN")), password=os.getenv("PASSWORD"), server=os.getenv("SERVER")) 
-    result = fx_agent.run_sync("Hey Felix, can you login to my metatrader app, and get the information of the pair BTCUSDm and place a trade if you find a trading opportunity", deps=dependencies)
+    result = fx_agent.run_sync("Hey Felix, can you login to my metatrader, get info on BTCUSDm and place a trade if you find an opportunity", deps=dependencies)
     print(result)
         # result = await fx_agent.run_sync("use_indicators(pair='BTCUSDm')")
         # print(result)
